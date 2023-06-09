2e897ce0c02a7d5c7d18b353ef8ce5a3596dd9d0
Nhom7CNPM <hanove025@gmail.com>
2023-06-09 09:30:22 +0700

    4 commit

--- app/src/main/java/com/example/project_b1_test/HomeActivity.java
+++ app/src/main/java/com/example/project_b1_test/HomeActivity.java
@@ -1,4 +1,25 @@
 package com.example.project_b1_test;
 
-public class HomeActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    Button test1butn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        test1butn =(Button) findViewById(R.id.test1btn);
+
+        test1butn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,StartTestActivity.class));
+            }
+        });
+    }
 }
