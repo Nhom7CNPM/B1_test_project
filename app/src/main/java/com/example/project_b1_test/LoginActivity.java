2869cb46cc95a77e0b4093e397dd5ce62230b365
Admin <bakuron1996@gmail.com>
2023-06-24 21:42:40 +0700

    Revert "24/06 D1 commit"
    This reverts commit d5c5846c372abaf3586f63c6629f6e9ccd72a9fc.

--- app/src/main/java/com/example/project_b1_test/LoginActivity.java
+++ app/src/main/java/com/example/project_b1_test/LoginActivity.java
@@ -2,54 +2,83 @@ package com.example.project_b1_test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.project_b1_test.db.DB_Login;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
+    DatabaseReference dbr = FirebaseDatabase.getInstance().getReferenceFromUrl("https://project-b1-test-default-rtdb.firebaseio.com/");
+    private FirebaseAuth auth;
     private EditText LoginEmail,LoginPassword;
     private Button LoginButton;
     private TextView SignUpReady;
-    DB_Login DB;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth=FirebaseAuth.getInstance();
 
         LoginEmail =findViewById(R.id.loginemail);
         LoginPassword = findViewById(R.id.loginpassword);
 
         LoginButton = findViewById(R.id.loginbtn);
         SignUpReady = findViewById(R.id.signupready);
-        DB = new DB_Login(this);
 
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = LoginEmail.getText().toString();
-                String password = LoginPassword.getText().toString();
+                String pass = LoginPassword.getText().toString();
+                if(!email.isEmpty() || !pass.isEmpty())
+                {
+                    dbr.child("email").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(email))
+                            {
+                                final String getPass = snapshot.child(email).child("pass").getValue(String.class);
+                                if (getPass.equals(pass))
+                                {
+                                    Toast.makeText(LoginActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                                else
+                                {
+                                    Toast.makeText(LoginActivity.this,"Đăng nhập không thành công",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error)
+                        {
 
-                if(email.equals("")||password.equals(""))
-                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
                 else
                 {
-                    Boolean checkuserpass = DB.checkusernamepassword(email, password);
-                    if(checkuserpass)
-                    {
-                        Toast.makeText(LoginActivity.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
-                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                    }
-                    else
-                    {
-                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(LoginActivity.this,"Điền đầy đủ thông tin",Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/project_b1_test/SignupActivity.java
+++ app/src/main/java/com/example/project_b1_test/SignupActivity.java
@@ -7,21 +7,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.project_b1_test.db.DB_Login;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class SignupActivity extends AppCompatActivity{
+
+    DatabaseReference dbr = FirebaseDatabase.getInstance().getReferenceFromUrl("https://project-b1-test-default-rtdb.firebaseio.com/");
+    private FirebaseAuth auth;
     private TextView BackSignIn;
     private EditText SignUpEmail, SignUpPass, ConfirmPass;
     private Button SignUpButton;
-    private DB_Login DB;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        DB = new DB_Login(this);
+        auth = FirebaseAuth.getInstance();
 
         SignUpEmail = findViewById(R.id.signupemail);
         SignUpPass = findViewById(R.id.signuppassword);
@@ -34,42 +46,45 @@ public class SignupActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 String email = SignUpEmail.getText().toString();
-                String password = SignUpPass.getText().toString();
-                String cpassword = ConfirmPass.getText().toString();
+                String pass = SignUpPass.getText().toString();
+                String cpass = ConfirmPass.getText().toString();
 
-                if(email.equals("")||password.equals("")||cpassword.equals(""))
-                    Toast.makeText(SignupActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else
+                if(email.isEmpty() || pass.isEmpty() || cpass.isEmpty())
                 {
-                    if(password.equals(cpassword))
-                    {
-                        Boolean checkuser = DB.checkusername(email);
-                        if(!checkuser)
-                        {
-                            Boolean insert = DB.insertData(email, password);
-                            if(insert)
+                    Toast.makeText(SignupActivity.this, "Vui lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                }
+                else if(pass.equals(cpass))
+                {
+                    dbr.child("email").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            // Kiểm tra tài khoản đã tồn tại
+                            if(snapshot.hasChild(email))
                             {
-                                Toast.makeText(SignupActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                                startActivity(intent);
+                                Toast.makeText(SignupActivity.this, "Tài khoản đã tồn tại", Toast.LENGTH_SHORT).show();
                             }
                             else
                             {
-                                Toast.makeText(SignupActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                                dbr.child("email").child(email).child("pass").setValue(pass);
+
+                                Toast.makeText(SignupActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                finish();
                             }
                         }
-                        else
-                        {
-                            Toast.makeText(SignupActivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
                         }
-                    }
-                    else
-                    {
-                        Toast.makeText(SignupActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
-                    }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(SignupActivity.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
         BackSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
